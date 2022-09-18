bac0861dd894c9f895af9e59605795e6e937e838
yonesJr <yones16jr@gmail.com>
2022-09-18 13:00:21 +0300

    fix error

--- lib/core/localization/en.dart
+++ lib/core/localization/en.dart
@@ -2,7 +2,6 @@ Map<String, String> en = {
   'home_page': 'Home Page',
   'Select_language': 'Select a language',
   'english': 'English',
-<<<<<<< HEAD
   'help':'Help',
   'FAQs' : 'FAQs',
   'Order' : 'Order',
@@ -13,23 +12,16 @@ Map<String, String> en = {
   'daddyEat':'daddyEat',
   'ready_to_go':'Are you ready to go?',
   'signin_now':'signin now',
-=======
   'login': 'login',
   'daddyEat': 'daddyEat',
   'ready_to_go': 'Are you ready to go?',
   'signin_now': 'signin now',
->>>>>>> 476edd75ce04ad82f25d6967232d458e4f0c56c0
   'iD_Number': 'ID Number',
   'password': 'Password',
   'remember_me': 'Remember me',
   'Login': 'Login',
   'signin': 'signin',
-<<<<<<< HEAD
-  'forgot_password':'forgot password',
   'click_here':'Click here',
 
-=======
   'forgot_password': 'forgot password?',
-  'click_here': 'Click here',
->>>>>>> 476edd75ce04ad82f25d6967232d458e4f0c56c0
 };
